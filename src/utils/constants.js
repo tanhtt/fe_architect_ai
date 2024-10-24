d9cdd7aa2fe3a17305c9dcdd758bbf83f16077aa
tanhtt <flywtan@gmail.com>
2024-10-24 09:39:56 +0700

    modify api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 let apiRoot = ''
 if(process.env.BUILD_MODE === 'dev'){
-    apiRoot = 'https://architectaiapp-ecf9apatbyatc4dx.canadacentral-01.azurewebsites.net'
+    apiRoot = 'http://localhost:8080'
 }
 if(process.env.BUILD_MODE === 'production'){
-    apiRoot = 'http://localhost:8080' //for deploy api
+    apiRoot = 'https://architectaiapp-ecf9apatbyatc4dx.canadacentral-01.azurewebsites.net'
 }
 export const API_ROOT = apiRoot
